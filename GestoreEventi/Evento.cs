79d23a02e92b81bba22b650df09f709b3746d010
lucianocecere07 <cecereluciano97@gmail.com>
2022-12-13 11:30:18 +0100

    Milestone: inserire nuovo evento con prenotazioni e numero di posti disponibili

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace GestoreEventi
 {
+    //--- Milestone 1 ---//
     public class Evento
     {
         //proprietà
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -11,6 +11,49 @@ II cliente necessita di un semplice programma senza interfaccia grafica (ossia c
 */
 
 
-//--- Milestone 1 ---//
+//--- Milestone 2 ---//
 
+//nuovo evento
+using GestoreEventi;
 
+Console.Write("inserisci il nome dell'evento: ");
+string titolo = Console.ReadLine();
+Console.Write("inserisci la data dell'evento (gg/mm/yyyy): ");
+DateTime data = DateTime.Parse(Console.ReadLine());
+Console.Write("inserisci il numero di posti totali: ");
+int capienzaMassima = int.Parse(Console.ReadLine());
+
+Console.Write("quanti posti desideri prenotare? ");
+int postiPrenotati = int.Parse(Console.ReadLine());
+
+Evento nuovoEvento = new Evento(titolo, data, capienzaMassima, postiPrenotati);
+
+//Stampare a video il numero di posti prenotati e quelli disponibili
+Console.WriteLine();
+Console.WriteLine("numero di posti prenotati = " + nuovoEvento.GetPostiPrenotati());
+int postiDisponibili = capienzaMassima - postiPrenotati;
+Console.WriteLine("numero di posti disponibili = " + postiDisponibili);
+
+//chiedere all'utente quanti posti vuole disdire, stampare i posti residui e quelli prenotati
+bool fineDisdetta = false;
+while (fineDisdetta == false)
+{
+    Console.WriteLine();
+    Console.Write("vuoi disdire dei posti? (si/no): ");
+    string inputUtente = Console.ReadLine();
+    if(inputUtente == "no")
+    {
+        Console.WriteLine("ok, va bene");
+        fineDisdetta = true;
+    }
+    else
+    {
+        Console.Write("indica il numero di posti da disdire: ");
+        int postiDisdetti = int.Parse(Console.ReadLine());
+        postiPrenotati = postiPrenotati - postiDisdetti;
+        postiDisponibili = postiDisponibili + postiDisdetti;
+    }
+    Console.WriteLine();
+    Console.WriteLine("numero di posti prenotati = " + postiPrenotati);
+    Console.WriteLine("numero di posti disponibili = " + postiDisponibili);
+}
